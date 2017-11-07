bed355a88e7c1d4b494ba3096b295a4d257c4de5
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-07 12:42:17 +0100

    created spare test

--- java/thebowlinggamekata/src/test/java/BowlingGameTest.java
+++ java/thebowlinggamekata/src/test/java/BowlingGameTest.java
@@ -1,33 +1,41 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BowlingGameTest {
+    private Game game;
 
-    @Test
-    public void createGame() throws Exception {
-        Game game = new Game();
+    @Before
+    public void setUp() {
+        game = new Game();
     }
 
     @Test
-    public void scoreGame() throws Exception {
-        Game game = new Game();
-        assertEquals(0, game.score());
+    public void initialScoreGame() throws Exception {
+        int score = game.score();
+        assertEquals(0, score);
     }
 
     @Test
     public void firstRoll() throws Exception {
-        Game game = new Game();
         game.roll(5);
         assertEquals(5, game.score());
     }
 
     @Test
     public void secondRoll() throws Exception {
-        Game game = new Game();
         game.roll(5);
         game.roll(4);
         assertEquals(9, game.score());
+    }
+
+    @Test
+    public void spare() throws Exception {
+        game.roll(6);
+        game.roll(4);
+        game.roll(5);
+        assertEquals(20, game.score());
     }
 
 
