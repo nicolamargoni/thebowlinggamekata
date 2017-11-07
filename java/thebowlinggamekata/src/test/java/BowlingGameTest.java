9dc6f4b648a9ff1d8362712d29b5d248e093fd50
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-07 11:15:08 +0100

    created score game test

--- java/thebowlinggamekata/src/test/java/BowlingGameTest.java
+++ java/thebowlinggamekata/src/test/java/BowlingGameTest.java
@@ -1,10 +1,19 @@
 import org.junit.Test;
 
-public class BowlingGameTest{
+import static org.junit.Assert.*;
+
+public class BowlingGameTest {
 
     @Test
-    public void createGame() throws Exception{
+    public void createGame() throws Exception {
         Game game = new Game();
     }
+
+    @Test
+    public void scoreGame() throws Exception {
+        Game game = new Game();
+        assertEquals(0, game.score());
+    }
+
 
 }
