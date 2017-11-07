53a0a65e8445c294b76f92ad704a342ce4d1f77e
nicolamargoni <nicola.margoni@xpeppers.com>
2017-11-07 15:46:51 +0100

    upgrade score() that calculate spare

--- java/thebowlinggamekata/src/main/java/Game.java
+++ java/thebowlinggamekata/src/main/java/Game.java
@@ -1,12 +1,54 @@
+import java.util.ArrayList;
+
 public class Game {
-    private int score = 0;
+    private int[] rools = new int[20];
+    private int roolsNumber = 0;
+
 
     public int score() {
-        return score;
+        int gameScore = 0;
+        int frameScore = 0;
+        int roll = 1;
+        boolean spare = false;
+
+        for (int i = 0; i < roolsNumber; i++) {
+            if (spare) {
+                frameScore += 2 * rools[i];
+            } else {
+                frameScore += rools[i];
+            }
+
+            if (isFirstRollOfFrame(roll)) {
+                if (isLastRool(i)) {
+                    gameScore += frameScore;
+                }
+                roll++;
+            } else {
+                if (isSpare(frameScore)) {
+                    spare = true;
+                }
+                roll--;
+                gameScore += frameScore;
+                frameScore = 0;
+            }
+        }
+        return gameScore;
     }
 
-
     public void roll(int pins) {
-        score += pins;
+        rools[roolsNumber] = pins;
+        roolsNumber++;
+    }
+
+    public boolean isSpare(int score) {
+        return score == 10;
+    }
+
+    public boolean isFirstRollOfFrame(int roll) {
+        return roll == 1;
+    }
+
+    public boolean isLastRool(int roll) {
+        return roll == (roolsNumber - 1);
     }
 }
